01eb24fad9da11343e657a4394beeaa8f8077707
Tomer Figenblat <tfigenbl@redhat.com>
2023-09-14 09:39:03 -0400

    chore: switched jul to jboss
    Signed-off-by: Tomer Figenblat <tfigenbl@redhat.com>

--- src/main/java/exate/gator/interceptor/TrafficInterceptor.java
+++ src/main/java/exate/gator/interceptor/TrafficInterceptor.java
@@ -9,6 +9,7 @@ import exate.gator.interceptor.api.ApiConfig;
 import exate.gator.interceptor.api.DatasetResponse;
 import exate.gator.interceptor.api.RequestHeaders;
 import exate.gator.interceptor.api.TokenResponse;
+import io.quarkus.logging.Log;
 import io.quarkus.vertx.web.Route;
 import io.vertx.core.Handler;
 import io.vertx.core.buffer.Buffer;
@@ -20,15 +21,10 @@ import io.vertx.ext.web.client.WebClient;
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-
 import java.util.Objects;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 @ApplicationScoped
 public class TrafficInterceptor {
-    private final static Logger LOG = Logger.getLogger(TrafficInterceptor.class.getName());
-
     private final WebClient client;
     private final ObjectMapper mapper;
     private final TargetConfig target;
@@ -49,12 +45,12 @@ public class TrafficInterceptor {
 
     @Route(regex = ".*")
     public void handle(RoutingContext context) {
-        LOG.fine(() -> String.format("got new request, %s", context.request().absoluteURI()));
+        Log.tracef("got new request, %s", context.request().absoluteURI());
         // create target request options using origin request and target host and port
         var targetRequestOpts = RequestOptionsFactory.createTargetOptions(target, context.request());
         // wrap target request
         var targetRequest = this.client.request(context.request().method(), targetRequestOpts);
-        LOG.info("proxying request to target");
+        Log.info("proxying request to target");
         targetRequest
             .ssl(this.target.secure())
             .sendBuffer(context.body().buffer())
@@ -64,13 +60,13 @@ public class TrafficInterceptor {
 
     private Handler<HttpResponse<Buffer>> handlerTargetResponse(RoutingContext context) {
         return targetResponse -> {
-            LOG.info("proxying request to target successful");
+            Log.info("proxying request to target successful");
             var bypass = context.request().getHeader(RequestHeaders.Api_Gator_Bypass.toString());
             if (Objects.nonNull(bypass) && bypass.equals("true")) {
-                LOG.info("bypassing gator and returning target response");
+                Log.info("bypassing gator and returning target response");
                 // end with the original target service response
                 context.response().end(targetResponse.bodyAsBuffer());;
-                LOG.info("bypassing gator and returning target response successful");
+                Log.info("bypassing gator and returning target response successful");
 
                 return;
             }
@@ -80,7 +76,7 @@ public class TrafficInterceptor {
             var tokenReqPayload = PayloadFactory.createTokenPayload(this.config);
             // create token request
             var tokenRequest = client.request(HttpMethod.POST, tokenReqOpts);
-            LOG.info("sending token request to gator");
+            Log.info("sending token request to gator");
             tokenRequest
                 .ssl(true)
                 .sendBuffer(Buffer.buffer(tokenReqPayload.toString()))
@@ -92,7 +88,7 @@ public class TrafficInterceptor {
 
     private Handler<HttpResponse<Buffer>> handleTokenResponse(RoutingContext context, HttpResponse<Buffer> targetResponse) {
         return tokenResponse -> {
-            LOG.info("sending token request to gator successful");
+            Log.info("sending token request to gator successful");
             TokenResponse parsedTokenResp = null;
             try {
                 parsedTokenResp = mapper.readValue(tokenResponse.bodyAsString(), TokenResponse.class);
@@ -106,7 +102,7 @@ public class TrafficInterceptor {
             var datasetReqPayload = PayloadFactory.createDatasetPayload(this.config, targetResponse);
             // create dataset request
             var datasetRequest = this.client.request(HttpMethod.POST, datasetReqOpts);
-            LOG.info("sending dataset request to gator");
+            Log.info("sending dataset request to gator");
             String datasetReqStr = null;
             try {
                 datasetReqStr = mapper.writeValueAsString(datasetReqPayload);
@@ -124,7 +120,7 @@ public class TrafficInterceptor {
 
     private Handler<HttpResponse<Buffer>> handleDatasetResponse(RoutingContext context, HttpResponse<Buffer> targetResponse) {
         return datasetResponse -> {
-            LOG.info("sending dataset request to gator successful");
+            Log.info("sending dataset request to gator successful");
             DatasetResponse parsedDatasetResp = null;
             try {
                 parsedDatasetResp = mapper.readValue(datasetResponse.bodyAsString(), DatasetResponse.class);
@@ -132,20 +128,19 @@ public class TrafficInterceptor {
                 handleErrors("failed parsing gator dataset response", 400, context).handle(e);
                 return;
             }
-            LOG.info("returning gator dataset as response");
+            Log.info("returning gator dataset as response");
             // include target response headers in gator'ed response
             targetResponse.headers().forEach((k, v) -> context.response().putHeader(k, v));
             context.response().putHeader(HttpHeaders.CONTENT_LENGTH, String.valueOf(parsedDatasetResp.dataSet().length()));
             // end with the gator'ed response
             context.response().end(Buffer.buffer(parsedDatasetResp.dataSet()));
-            LOG.info("returning gator dataset as response successful");
+            Log.info("returning gator dataset as response successful");
         };
     }
 
     private Handler<Throwable> handleErrors(String msg, int code, RoutingContext context) {
         return t -> {
-            LOG.severe(msg);
-            LOG.log(Level.FINE, t, t::getMessage);
+            Log.error(msg, t);
             context.response().setStatusCode(code);
             context.response().end(msg);
         };
