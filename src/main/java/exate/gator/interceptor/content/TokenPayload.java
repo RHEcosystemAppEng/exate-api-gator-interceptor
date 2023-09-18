1df3d2e7b1a2dd2867a4e0bbe04a19a59b41f8e2
Tomer Figenblat <tfigenbl@redhat.com>
2023-09-18 08:50:20 -0400

    fix: token form paramaters should be url encoded
    Signed-off-by: Tomer Figenblat <tfigenbl@redhat.com>

--- src/main/java/exate/gator/interceptor/content/TokenPayload.java
+++ src/main/java/exate/gator/interceptor/content/TokenPayload.java
@@ -1,11 +1,20 @@
 package exate.gator.interceptor.content;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
 /**
  * Record for serializing the payload for sending TOKEN requests to API-Gator.
  * Use the toString method for FORMS content.
  */
 public record TokenPayload(String clientId, String clientSecret, String grantType) {
     /** Returns the string representation of the request body for usage with FORM-like content types */
+    public TokenPayload(String clientId, String clientSecret, String grantType) {
+        this.clientId = URLEncoder.encode(clientId, StandardCharsets.UTF_8);
+        this.clientSecret = URLEncoder.encode(clientSecret, StandardCharsets.UTF_8);
+        this.grantType = URLEncoder.encode(grantType, StandardCharsets.UTF_8);
+    }
+
     @Override
     public String toString() {
         return String.format("client_id=%s&client_secret=%s&grant_type=%s", clientId, clientSecret, grantType);
